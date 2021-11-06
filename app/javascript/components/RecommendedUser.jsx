ea9764f83e13489eb5835b6757124ba5967f3834
Lily <lolpo12345@outlook.com>
2021-11-06 18:09:04 +0300

    Style recommendation

--- app/javascript/components/RecommendedUser.jsx
+++ app/javascript/components/RecommendedUser.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RecommendedUser = (props) => {
+    return(
+        <Link to={`/user/${props.user.id}`} style={{textDecoration:"none", color:"black"}} >
+          <div className="my-1 border" style={{display: "flex", justifyContent: "space-between"}}>  
+            <img src={props.image} className="border-right-0 border-dark" style={{width: 40}}/>
+            <h3>{props.user.username}</h3>
+          </div>
+        </Link>
+    )
 
-  return(
-    <div>
-      
-      <Link to={`/user/${props.user.id}`} style={{textDecoration:"none", color:"black"}} >
-        <img src={props.image} style={{width: 50 }}/>
-        <h3>{props.user.username}</h3>
-      </Link>
-    </div>
-  )
 };
 
 export default RecommendedUser;
