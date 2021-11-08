9a961a2d23475888602c886c63614854f8deb36a
Lily <lolpo12345@outlook.com>
2021-11-08 20:58:07 +0300

    Implement relations between comment and post

---
+++ app/javascript/components/PostPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostPage = (props) => {
+  return(
+    <h1>You're on PostPage!</h1>
+  )
+};  
--- app/models/twat.rb
+++ app/models/twat.rb
@@ -1,5 +1,7 @@
 class Twat < ApplicationRecord
-  belongs_to :user
-  has_many :likes
   validates :body, length: { minimum: 1, maximum: 250 }
+  belongs_to :user
+  has_many :comments
+  has_many :likes
+  
 end
