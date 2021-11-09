7b3856a20375a04f908363add66d8d7f4e4cca57
Lily <lolpo12345@outlook.com>
2021-11-09 16:42:12 +0300

    Implement deleting function

--- app/javascript/components/Comment.jsx
+++ app/javascript/components/Comment.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link} from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Comment = (props) => {
 
 
+  const handleDelete = () => {
+    fetch(`/comments/${props.comment.id}`, {
+      method: 'DELETE'
+    })
+  };
+
   return(
     <div className="row justify-content-center">
-      <div className="card col-md-6 m-4 w-50">
+      <div className="card col-md-6 m-4">
         <img src={props.comment.image} className="w-25" />
         <h3><Link style={{textDecoration:"none"}} to={`/user/${props.comment.user_id}`}>@{props.comment.poster}</Link></h3>
-        <h3 style={{cursor:"pointer"}}>{props.comment.body}</h3>
+        <h3>{props.comment.body}</h3>
+
+        <Button className="m-1" onClick={handleDelete}>Delete Comment</Button>
       </div>
     </div>
   )
