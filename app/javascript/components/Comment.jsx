1b4c785e5674421ef286f41eb31bf405f44f97b0
Lily <lolpo12345@outlook.com>
2021-11-09 16:15:51 +0300

    Fix positioning for Comment

--- app/javascript/components/Comment.jsx
+++ app/javascript/components/Comment.jsx
@@ -5,10 +5,12 @@ const Comment = (props) => {
 
 
   return(
-    <div className="card col-md-6 m-4">
-      <img src={props.comment.image} className="w-25" />
-      <h3><Link style={{textDecoration:"none"}} to={`/user/${props.comment.user_id}`}>@{props.comment.poster}</Link></h3>
-      <h3 style={{cursor:"pointer"}}>{props.comment.body}</h3>
+    <div className="row justify-content-center">
+      <div className="card col-md-6 m-4 w-50">
+        <img src={props.comment.image} className="w-25" />
+        <h3><Link style={{textDecoration:"none"}} to={`/user/${props.comment.user_id}`}>@{props.comment.poster}</Link></h3>
+        <h3 style={{cursor:"pointer"}}>{props.comment.body}</h3>
+      </div>
     </div>
   )
 };
