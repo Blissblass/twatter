2abca9ef03f7876d3f42cc0087e92b9273bd84cb
Lily <lolpo12345@outlook.com>
2021-12-16 22:10:17 +0300

    Implement loading for followRecommendations

--- app/javascript/components/FollowRecommendations.jsx
+++ app/javascript/components/FollowRecommendations.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import RecommendedUser from './RecommendedUser';
 import UserContext from "./Contexts/UserContext";
 import { useContext } from "react";
+import { Spinner } from 'react-bootstrap';
 
 const FollowRecommendations = (props) => {
   const { currUser } = useContext(UserContext);
 
+  const [loading, setLoading] = useState(true);
   const [usersData, setUsersData] = useState([]);
   useEffect(()=> {
     const CSRF = document.querySelector("meta[name='csrf-token']").getAttribute("content");
@@ -21,21 +23,27 @@ const FollowRecommendations = (props) => {
       })
     })
     .then(data => data.json())
-    .then(data => setUsersData(data));
+    .then(data => {
+      setUsersData(data);
+      setLoading(false);
+    });
   }, []);
 
   return(
     <div className="col-md-2 mt-2 mx-5">
-      <div className="card p-1 text-center" style={{width:250}}>
-        <h3>Cool People</h3>
+        <div className="card p-1 text-center" style={{width:250}}>
+          <h3>Cool People</h3>
 
-        {usersData[0] ? 
-          usersData.map(data => <RecommendedUser key={data.user.id} user={data.user} image={data.image} />) 
-        :
-          <h4>No recommendations available :/</h4>
-        }
+          {loading ? 
+            <Spinner animation="border" variant="primary" className="mx-auto mt-2" style={{width: 50, height: 50}} />
+          :
+            usersData[0] ? 
+              usersData.map(data => <RecommendedUser key={data.user.id} user={data.user} image={data.image} />) 
+            :
+              <h4>No recommendations available :/</h4>
+          }
 
-      </div>
+        </div>
     </div>
   )
 };
